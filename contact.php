434575b9ff6461303cdc7abd16aee2192211e712
Siddhesh172004 <84581657+Siddhesh172004@users.noreply.github.com>
2021-05-22 02:36:37 +0530

    Add files via upload

---
+++ contact.php
@@ -0,0 +1,18 @@
+<?php 
+    $name = $_POST['Username'];
+    $email = $_POST['Useremail'];
+    $message = $_POST['message'];
+
+    $email_from = 'jondhale172004@gmail.com';
+    $email_subject = 'Thanks for contacting us';
+    $email_body = "User name : $name.\n";
+                    "User Email : $email.\n";
+                       "User Message : $message.\n";
+
+    $to = "jondhale172004@gmail.com";
+    $headers = "From:$email_from \r\n"; 
+    $headers = "Reply-To: $email \r\n";
+    
+    mail($to,$email_subject,$email_body,$headers);
+    header("Location: index.html");
+?>
