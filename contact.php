9d0335ed632a3a0ee9992ab6ce12d56a951016e6
Siddhesh172004 <84581657+Siddhesh172004@users.noreply.github.com>
2021-05-22 02:54:02 +0530

    Update contact.php

--- contact.php
+++ contact.php
@@ -17,3 +17,14 @@
     header("Location:thank.html");
 
 ?>
+
+<?php $name = $_POST['Username'];
+$email = $_POST['Useremail'];
+$message = $_POST['message'];
+$formcontent="From: $name \n Message: $message";
+$recipient = "jondhale172004@gmail.com";
+$subject = "Thanks for contacting us";
+$mailheader = "From: $email \r\n";
+mail($recipient, $subject, $formcontent, $mailheader) or die("Error!");
+echo "Thank You!";
+?>
